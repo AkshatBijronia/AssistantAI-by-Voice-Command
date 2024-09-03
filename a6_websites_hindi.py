869710c1a3be586e2284ebddfcd106916824e6b6
Akshat Bijronia <111517710+AkshatBijronia@users.noreply.github.com>
2024-09-03 09:19:46 +0530

    Add files via upload
    This project is a Python-based voice assistant that can perform various tasks based on voice commands. It leverages several Python libraries and APIs to recognize speech, process natural language, and execute commands such as web searches, opening applications, and providing information.
    
    Features
    Voice Recognition: Converts spoken language into text using the SpeechRecognition library.
    Text-to-Speech: Provides spoken responses using the pyttsx3 library.
    Natural Language Processing: Understands and processes user commands.
    Task Automation: Can perform tasks like opening websites, retrieving weather information, and more.
    Extensible: Easily add new commands and functionalities.
    How It Works
    Speech Recognition: Listens for voice commands and converts them into text.
    Command Processing: Analyzes the text to determine the appropriate action.
    Task Execution: Executes the command, such as performing a web search or opening an application.
    Response Generation: Provides a spoken response to the user.

---
+++ a6_websites_hindi.py
@@ -0,0 +1,56 @@
+# http://qiniucdn.star-lai.cn/portal/2016/09/05/tu3p2vd4znn
+from gtts import gTTS
+import pyttsx3
+import speech_recognition as sr
+import webbrowser
+import os
+import openai
+import datetime
+# def say(text):
+#     engine = pyttsx3.init()
+#     engine.say(text)
+#     engine.runAndWait()
+    
+def say(text, lang='en'):
+    tts = gTTS(text=text, lang=lang)
+    tts.save("temp.mp3")
+    os.system("start temp.mp3") 
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+         print("Recognizing..... ")
+         query = r.recognize_google(audio, language ="hi")
+         print(f"User said: {query}")
+         return query
+        except Exception as e:
+         return "some error occured, Sorry from Jarvis"
+
+if __name__ == '__main__':
+    print("python")
+    say("हेलो मैं जार्विस ए i बोल रहा हूं आपको मेरे से क्या काम है",lang='hi')
+    while True:
+      print("Listening.....")
+      query = takeCommand()
+      sites = [["youtube", "https://www.youtube.com"],["wikipedia","https://www.wikipedia.org/"],["linkedin","https://www.linkedin.com/feed/"],["यूट्यूब खोलो","https://www.youtube.com"],["विकिपीडिया खोलो","https://www.wikipedia.org/"],["लिंकडइन खोलो","https://www.linkedin.com/feed/"]]  
+      for site in sites:
+       if f"Open {site[0]}".lower() in query.lower():
+            say(f"Opening {site[0]} Sir... ")
+            webbrowser.open(site[1])
+       if "open movie" in query:
+            musicPath = "D:\ALL MOVIES\HOLLYWOOD MOVIES\www.9xflix.com - Iron Man 3 (2013) Dual Audio Hindi 480p BluRay.mkv"
+            os.startfile(musicPath)
+       if "फिल्म दिखाओ" in query:
+            musicPath = "D:\ALL MOVIES\HOLLYWOOD MOVIES\www.9xflix.com - Iron Man 3 (2013) Dual Audio Hindi 480p BluRay.mkv"
+            os.startfile(musicPath)
+       if "the time" in query:
+            strfTime = datetime.datetime.now().strftime("%H:%M:%S")
+            say(f"Sir the time is {strfTime}")
+       if "समय बताओ" in query:
+            hour = datetime.datetime.now().strftime("%H")
+            minutes = datetime.datetime.now().strftime("%M")
+            seconds = datetime.datetime.now().strftime("%S")
+            say(f"Sir the time is {hour} bazzgjkaar {minutes} auor {seconds} seconds hoo rahaa hai")
